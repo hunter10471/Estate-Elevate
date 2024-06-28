1ed30f528bf9271aa408c62a307181f773d0497c
hunter10471 <rafayzia10@gmail.com>
2024-06-29 00:03:11 +0500

    Fixed middleware bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,34 @@
 import { UserType } from "@prisma/client";
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
-	// `withAuth` augments your `Request` with the user's token.
 	function middleware(req) {
-		const isOnAdminPanel = req.nextUrl.pathname.startsWith("/admin");
 		const isOnEditProfile = req.nextUrl.pathname.startsWith("/profile");
 		const isOnLogin = req.nextUrl.pathname.startsWith("/auth/login");
 		const isOnRegister = req.nextUrl.pathname.startsWith("/auth/signup");
 		const token = req.nextauth.token;
-		if (isOnAdminPanel && token?.userType !== UserType.USER) {
-			return NextResponse.redirect("/");
-		}
-		if (isOnEditProfile && token) {
-			return NextResponse.redirect("/auth/login");
+		if (isOnEditProfile && !token) {
+			return NextResponse.redirect(new URL("/auth/login", req.url));
 		}
 		if ((isOnLogin || isOnRegister) && token) {
-			return NextResponse.redirect("/");
+			return NextResponse.redirect(new URL("/", req.url));
 		}
 		return NextResponse.next();
+	},
+	{
+		callbacks: {
+			authorized({ req, token }) {
+				const isOnAdminPanel = req.nextUrl.pathname.startsWith("/admin");
+				if (token?.userType !== UserType.ADMIN && isOnAdminPanel) {
+					return false;
+				}
+				return true;
+			},
+		},
 	}
 );
 
-export const config = { matcher: ["/"] };
+export const config = {
+	matcher: ["/admin", "/profile", "/auth/login", "/auth/signup"],
+};
